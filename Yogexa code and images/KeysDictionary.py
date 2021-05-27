7692118683e3d18cf90cb43b4604c43fa5802767
technocrat195 <84908665+technocrat195@users.noreply.github.com>
2021-05-27 16:22:58 +0530

    Add files via upload

---
+++ Yogexa code and images/KeysDictionary.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+keys_Action={"back pain":["imgSpch","Backpain is very common among people all around the globe and the main yoga excercises for backpain is Cobra pose,Locust pose,Bridge pose etc.","locus pose"], 
+             "stress":["imgSpch","If you have a lot of stress then yoga excercises are the solutions for you like Head to Knee forward bend,Cat-Cow pose,Corpse pose etc","stress relief pose"], 
+             
+             "weight":["imgSpch","If your goal is weight loss, choosing one of the vigorous, flowing styles of yoga, such as Ashtanga, Vinyasa or Power Yoga, is the smart choice.","weight loss yoga"],
+             "relaxation":["imgSpch","Yoga is a mind-body practice that combines physical poses, controlled breathing, and meditation or relaxation. Yoga may help reduce stress, lower blood pressure and lower your heart rate. And almost anyone can do it.","relaxation pose"],
+             
+             "exit":["exit","Goodbye for now and I hope that we meet again to discuss about yoga"],
+             "close":["exit","This conversation was very delighting and I hope you learnt more about yoga today Goodbye"]
+             
+             }
